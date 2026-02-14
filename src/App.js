996910f624e95e7c39d50700594e6b3c3b36ba5c
cxgowda <chirfeb@gmail.com>
2026-02-14 16:04:00 +0530

    patch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Game from "./Game";
 import Scoreboard from "./Scoreboard";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Game />} />
         <Route path="/scoreboard" element={<Scoreboard />} />
       </Routes>
     </Router>
   );
-}
-
+} 
 export default App;
